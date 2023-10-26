b1f97501ac0f8d33c02cbb914fb8261aa7bca623
Sohel Bargir <110667879+sohel4040@users.noreply.github.com>
2023-10-26 12:04:31 +0530

    Add files via upload

---
+++ my.py
@@ -0,0 +1,8 @@
+a = int(input("Enter a first number: ")
+b = int(input("Enter a second number: ")
+
+print("Addition is", a + b)
+print("Multiplication is", a * b)
+print("Division is", a / b)
+print("Subtraction is", a - b)
+
